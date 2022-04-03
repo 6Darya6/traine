ed35fde3c06323cbbdbbb43c15cb8e7950a1ae19
Dasha Rynkevich <dasarynkevic@gmail.com>
2022-04-03 19:52:22 +0300

    releas 1

---
+++ test1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            double r = Convert.ToDouble(Console.ReadLine());
+            double rez = pi * r*r;
+            Console.WriteLine("rezultat raven {0}", rez);
+        }
+    }
+}
